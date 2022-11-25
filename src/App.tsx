ca9470cd10870cfabbf2766b211100ee633d0659
Shahid Nauman <shahidnauman@msn.com>
2022-11-25 19:31:01 +0500

    Datatables for MUI with Customize Columns

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,57 @@ function App() {
     filterType: "checkbox",
   };
 
+  const columnsCustom: MUIDataTableColumnDef[] = [
+    {
+      name: "name",
+      label: "Name",
+      options: {
+        filter: true,
+        sort: true,
+      },
+    },
+    {
+      name: "company",
+      label: "Company",
+      options: {
+        filter: true,
+        sort: false,
+      },
+    },
+    {
+      name: "city",
+      label: "City",
+      options: {
+        filter: true,
+        sort: false,
+      },
+    },
+    {
+      name: "state",
+      label: "State",
+      options: {
+        filter: true,
+        sort: false,
+      },
+    },
+  ];
+
+  const dataCustom: Array<object | number[] | string[]> = [
+    { name: "Joe James", company: "Test Corp", city: "Yonkers", state: "NY" },
+    { name: "John Walsh", company: "Test Corp", city: "Hartford", state: "CT" },
+    { name: "Bob Herm", company: "Test Corp", city: "Tampa", state: "FL" },
+    {
+      name: "James Houston",
+      company: "Test Corp",
+      city: "Dallas",
+      state: "TX",
+    },
+  ];
+
+  const optionsCustom: MUIDataTableOptions = {
+    filterType: "checkbox",
+  };
+
   return (
     <div className='App'>
       <MUIDataTable
@@ -26,6 +77,12 @@ function App() {
         data={data}
         columns={columns}
         options={options}
+      />
+      <MUIDataTable
+        title={"Employee List"}
+        data={dataCustom}
+        columns={columnsCustom}
+        options={optionsCustom}
       />
     </div>
   );
